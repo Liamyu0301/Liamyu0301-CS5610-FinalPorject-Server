09af77d852835fe71c32b602270619c475f9b624
Gigi Breton <breton.b@northeastern.edu>
2024-11-30 20:55:10 -0500

    updated quiz schema

--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -4,39 +4,51 @@ const schema = new mongoose.Schema(
         course: { type: mongoose.Schema.Types.ObjectId, ref: "CourseModel" },
         title: String,
         description: String,
-        assignTo: String,
+        points: Number,
         type: {
             type: String,
-            enum: ["Graded Quiz", "Ungraded Quiz"],
+            enum: ["Graded Quiz", "Practice Quiz", "Graded Survey", "Ungraded Survey"],
             default: "Graded Quiz",
             required: true
         },
-        points: Number,
         group: {
             type: String,
-            enum: ["ASSIGNMENTS", "QUIZZES"],
+            enum: ["ASSIGNMENTS", "QUIZZES", "EXAMS", "PROJECTS"],
             default: "QUIZZES",
             required: true
         },
-        shuffleAnswers: { type: Boolean, default: false },
-        timeLimit: { type: Boolean, default: false },
-        multipleAttempts: { type: Boolean, default: false },
-        showCorrectAnswers: { type: Boolean, default: true },
-        accessCode: { type: Boolean, default: false },
-        oneQuestionAtATime: { type: Boolean, default: false },
-        webcamReq: { type: Boolean, default: false },
-        lockQuestions: { type: Boolean, default: false },
-        displayGradeAs: {
+        options: {
+            shuffleAnswers: { type: Boolean, default: false },
+            timeLimit: { type: Boolean, default: false },
+            timeLimitMinutes: Number,
+            multipleAttempts: { type: Boolean, default: false },
+            showCorrectAnswers: { type: Boolean, default: true },
+            showCorrectAnswersTime: {
+                type: String,
+                enum: ["Immediately", "One Day", "One Week"],
+                default: "Immediately",
+                required: true
+            },
+            displayGradeAs: {
+                type: String,
+                enum: ["Percentage", "Number"],
+                default: "Percentage",
+                required: true
+            },
+            accessCode: { type: Boolean, default: false },
+            oneQuestionAtATime: { type: Boolean, default: false },
+            webcamReq: { type: Boolean, default: false },
+            lockQuestions: { type: Boolean, default: false }
+        },
+        assignTo: {
             type: String,
-            enum: ["Percentage", "Number"],
-            default: "Percentage",
+            enum: ["Everyone"],
+            default: "Everyone",
             required: true
         },
         dueDate: Date,
         availableDate: Date,
         availableUntil: Date,
-        availableFromText: String,
-        dueDateText: String,
     },
     { collection: "quizzes" }
 );
